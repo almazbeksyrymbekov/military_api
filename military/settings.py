3dcbbe9c5fa23b50fa42453d7cc2b4f0d1710bd2
almazbeksyrymbekov <almazbeksyrymbekov@gmail.com>
2024-12-18 21:11:45 +0600

    cors error

--- military/settings.py
+++ military/settings.py
@@ -42,9 +42,12 @@ INSTALLED_APPS = [
     'army',
     'drf_spectacular',
     'drf_spectacular_sidecar',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'military.urls'
 
